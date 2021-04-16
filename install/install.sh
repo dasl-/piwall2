42dcd2ff08ecbfe29366f9028ee6447ee36ed886
dasl- <david.leibovic@gmail.com>
2021-04-16 03:24:46 -0400

    doc

--- install/install.sh
+++ install/install.sh
@@ -4,6 +4,7 @@ set -eou pipefail
 
 main(){
     updateAndInstallPackages
+    disableWifi
 }
 
 updateAndInstallPackages(){
@@ -12,6 +13,11 @@ updateAndInstallPackages(){
     sudo apt -y install ffmpeg vlc omxplayer python3-pip
     sudo apt -y full-upgrade
     sudo pip3 install --upgrade youtube_dl
+}
+
+disableWifi(){
+    # TODO: automate setting this: https://raspberrypi.stackexchange.com/a/62522
+    # dtoverlay=disable-wifi
 }
 
 main
