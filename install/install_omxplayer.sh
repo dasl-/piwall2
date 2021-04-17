b894f2e6a941a325381606170b1df9a2263ecb62
dasl- <david.leibovic@gmail.com>
2021-04-17 00:21:52 -0400

    build omx

--- install/install_omxplayer.sh
+++ install/install_omxplayer.sh
@@ -4,12 +4,16 @@ set -eou pipefail
 cd /home/pi
 git clone https://github.com/popcornmix/omxplayer.git
 cd omxplayer
-./prepare-native-raspbian.sh
-sudo apt-get update && sudo apt install -y git libasound2-dev libva2 libpcre3-dev libidn11-dev libboost-dev libdbus-1-dev libssh-dev libssl-dev libsmbclient-dev libavutil-dev libavcodec-dev libavformat-dev libswscale-dev
 
 # see https://github.com/popcornmix/omxplayer/issues/731
+sed -i -e 's/git-core/git/g' prepare-native-raspbian.sh
+sed -i -e 's/libva1/libva2/g' prepare-native-raspbian.sh
+sed -i -e 's/libssl1.0-dev/libssl-dev/g' prepare-native-raspbian.sh
 sed -i -e 's/--enable-libsmbclient/--disable-libsmbclient/g' Makefile.ffmpeg
+
+./prepare-native-raspbian.sh
 make ffmpeg
+sudo apt-get update && sudo apt install -y git libasound2-dev libva2 libpcre3-dev libidn11-dev libboost-dev libdbus-1-dev libssh-dev libsmbclient-dev libssl1.0-dev #libavutil-dev libavcodec-dev libavformat-dev libswscale-dev
 
 # see https://github.com/popcornmix/omxplayer/commit/6d186be9d15c3d2ee8a4256afd26cddebbd8251e
 # https://www.raspberrypi.org/forums/viewtopic.php?t=258647
