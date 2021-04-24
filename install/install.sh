2a9fa4ad073838353a418d03588f19bd79fa54c5
dasl- <david.leibovic@gmail.com>
2021-04-24 02:34:18 -0400

    harden install script and add ability to enable composite video output

--- install/install.sh
+++ install/install.sh
@@ -2,9 +2,17 @@
 
 set -eou pipefail
 
+CONFIG=/boot/config.txt
+is_restart_required=false
+
 main(){
     updateAndInstallPackages
     disableWifi
+
+    if [ "$is_restart_required" = true ] ; then
+        echo "Restarting..."
+        sudo shutdown -r now
+    fi
 }
 
 updateAndInstallPackages(){
@@ -15,14 +23,39 @@ updateAndInstallPackages(){
     sudo pip3 install --upgrade youtube_dl
 }
 
+# see: https://raspberrypi.stackexchange.com/a/62522
 disableWifi(){
-    # see: https://raspberrypi.stackexchange.com/a/62522
-    if ! grep -q '^dtoverlay=disable-wifi' /boot/config.txt ; then
+    if ! grep -q '^dtoverlay=disable-wifi' $CONFIG ; then
         echo 'disabling wifi...'
-        echo 'dtoverlay=disable-wifi' | sudo tee -a /boot/config.txt >/dev/null
-        # TODO : reboot if wifi disabled
+
+        # uncomment it if the stanza is commented out
+        sudo sed $CONFIG -i -e "s/^#\?dtoverlay=disable-wifi *$/dtoverlay=disable-wifi/"
+
+        # create the stanza if it doesn't yet exist
+        if ! grep -q '^dtoverlay=disable-wifi' $CONFIG ; then
+            echo 'dtoverlay=disable-wifi' | sudo tee -a $CONFIG >/dev/null
+        fi
+        is_restart_required=true
     else
         echo 'wifi already disabled...'
+    fi
+}
+
+# see: https://www.raspberrypi.org/documentation/configuration/config-txt/video.md
+enableCompositeVideoOutput(){
+    if ! grep -q '^enable_tvout=1' $CONFIG ; then
+        echo 'enabling composite video output...'
+
+        # uncomment it and enable it if the stanza is commented out
+        sudo sed $CONFIG -i -e "s/^#\?enable_tvout=.*/enable_tvout=1/"
+
+        # create the stanza if it doesn't yet exist
+        if ! grep -q '^enable_tvout=1' $CONFIG ; then
+            echo 'enable_tvout=1' | sudo tee -a $CONFIG >/dev/null
+        fi
+        is_restart_required=true
+    else
+        echo 'composite video output already enabled...'
     fi
 }
 
