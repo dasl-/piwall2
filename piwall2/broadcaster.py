01712827ce7186c3caddd5461e54c31b5c26405a
dasl- <david.leibovic@gmail.com>
2021-06-23 00:41:00 -0400

    fix syntax

--- piwall2/broadcaster.py
+++ piwall2/broadcaster.py
@@ -1,9 +1,5 @@
-import socket
-import struct
-import sys
 import shlex
-
-from piwall2.broadcaster import Broadcaster
+import subprocess
 
 class Broadcaster:
 
@@ -14,7 +10,7 @@ class Broadcaster:
         pass
 
     def broadcast(self, video_url):
-        cmd = 
+        cmd = (
             # Get eth0 IP
             "eth0_ip=$(ip -json -pretty addr show eth0 | jq -c --raw-output '.[] | " + 
                 "select(.ifname != null) | select(.ifname | contains(\"eth0\")) | .addr_info | .[] | " + 
@@ -25,6 +21,7 @@ class Broadcaster:
                 f"-i <(youtube-dl {shlex.quote(video_url)} -f 'bestvideo[vcodec^=avc1][height<=720]' -o -) " +
                 f"-i <(youtube-dl {shlex.quote(video_url)} -f 'bestaudio' -o -) " +
                 "-c:v copy -c:a aac -f matroska 'udp://239.0.1.23:1234?localaddr=$eth0_ip'"
+        )
         proc = subprocess.Popen(
             cmd, shell = True, executable = '/usr/bin/env bash', start_new_session = True
         )
--- piwall2/receiver.py
+++ piwall2/receiver.py
@@ -2,6 +2,7 @@ import socket
 import struct
 import sys
 import shlex
+import subprocess
 
 from piwall2.broadcaster import Broadcaster
 
@@ -16,14 +17,14 @@ class Receiver:
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
         sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         sock.bind((Broadcaster.MULTICAST_ADDRESS, Broadcaster.MULTICAST_PORT))
-        mreq = struct.pack("4sl", socket.inet_aton(MCAST_GRP), socket.INADDR_ANY)
+        mreq = struct.pack("4sl", socket.inet_aton(Broadcaster.MULTICAST_ADDRESS), socket.INADDR_ANY)
         sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
 
         has_set_timeout = False
 
         cmd = f"omxplayer --crop {shlex.quote(crop_string)} -o local --no-keys --threshold 3 pipe:0"
         proc = subprocess.Popen(
-            cmd, shell = True, executable = '/usr/bin/env bash', start_new_session = True, stdin = PIPE
+            cmd, shell = True, executable = '/usr/bin/env bash', start_new_session = True, stdin = subprocess.PIPE
         )
         while True:
             ret = sock.recv(4096)
