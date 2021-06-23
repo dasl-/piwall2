8e13671169b8906540feca6f924f6e6cdf7fb26f
dasl- <david.leibovic@gmail.com>
2021-06-23 00:52:58 -0400

    fix syntax
    Former-commit-id: e05306c7c7c61b3d2f719e27b03106c116c2716e

--- piwall2/broadcaster.py
+++ piwall2/broadcaster.py
@@ -12,15 +12,15 @@ class Broadcaster:
     def broadcast(self, video_url):
         cmd = (
             # Get eth0 IP
-            "eth0_ip=$(ip -json -pretty addr show eth0 | jq -c --raw-output '.[] | " + 
-                "select(.ifname != null) | select(.ifname | contains(\"eth0\")) | .addr_info | .[] | " + 
+            "eth0_ip=$(ip -json -pretty addr show eth0 | jq -c --raw-output '.[] | " +
+                "select(.ifname != null) | select(.ifname | contains(\"eth0\")) | .addr_info | .[] | " +
                 "select(.family == \"inet\") | .local') ; " +
-            
+
             # Mix the best audio with the video and send via multicast
-            "ffmpeg -re " + 
+            "ffmpeg -re " +
                 f"-i <(youtube-dl {shlex.quote(video_url)} -f 'bestvideo[vcodec^=avc1][height<=720]' -o -) " +
                 f"-i <(youtube-dl {shlex.quote(video_url)} -f 'bestaudio' -o -) " +
-                "-c:v copy -c:a aac -f matroska 'udp://239.0.1.23:1234?localaddr=$eth0_ip'"
+                "-c:v copy -c:a aac -f matroska \"udp://239.0.1.23:1234?localaddr=$eth0_ip\""
         )
         proc = subprocess.Popen(
             cmd, shell = True, executable = '/usr/bin/bash', start_new_session = True
