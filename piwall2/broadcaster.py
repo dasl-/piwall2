62ecdf79eddde90f3a9efb6491abbfa49eafe336
dasl- <david.leibovic@gmail.com>
2021-06-23 00:31:44 -0400

    classes
    Former-commit-id: ac60526169d015752482808c3fa91ceb2523e97f

---
+++ piwall2/broadcaster.py
@@ -0,0 +1,30 @@
+import socket
+import struct
+import sys
+import shlex
+
+from piwall2.broadcaster import Broadcaster
+
+class Broadcaster:
+
+    MULTICAST_ADDRESS = '239.0.1.23'
+    MULTICAST_PORT = 1234
+
+    def __init__(self):
+        pass
+
+    def broadcast(self, video_url):
+        cmd = 
+            # Get eth0 IP
+            "eth0_ip=$(ip -json -pretty addr show eth0 | jq -c --raw-output '.[] | " + 
+                "select(.ifname != null) | select(.ifname | contains(\"eth0\")) | .addr_info | .[] | " + 
+                "select(.family == \"inet\") | .local') ; " +
+            
+            # Mix the best audio with the video and send via multicast
+            "ffmpeg -re " + 
+                f"-i <(youtube-dl {shlex.quote(video_url)} -f 'bestvideo[vcodec^=avc1][height<=720]' -o -) " +
+                f"-i <(youtube-dl {shlex.quote(video_url)} -f 'bestaudio' -o -) " +
+                "-c:v copy -c:a aac -f matroska 'udp://239.0.1.23:1234?localaddr=$eth0_ip'"
+        proc = subprocess.Popen(
+            cmd, shell = True, executable = '/usr/bin/env bash', start_new_session = True
+        )
