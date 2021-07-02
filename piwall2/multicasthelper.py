97fe3ebc102ff87304274975cef6f647448330d5
dasl- <david.leibovic@gmail.com>
2021-07-01 21:04:39 -0400

    set socket receive buffer sizes to prevent udp packet loss
    Former-commit-id: fb24c61981c73be01386bcbdb739580f74823d5a

--- piwall2/multicasthelper.py
+++ piwall2/multicasthelper.py
@@ -1,5 +1,6 @@
 import socket
 import struct
+import subprocess
 from piwall2.logger import Logger
 
 class MulticastHelper:
@@ -7,12 +8,6 @@ class MulticastHelper:
     ADDRESS = '239.0.1.23'
     VIDEO_PORT = 1234
     CONTROL_PORT = 1235
-
-    # regarding socket.IP_MULTICAST_TTL
-    # ---------------------------------
-    # for all packets sent, after two hops on the network the packet will not
-    # be re-sent/broadcast (see https://www.tldp.org/HOWTO/Multicast-HOWTO-6.html)
-    TTL = 2
 
     # Message will be sent according to the control protocol over the control port.
     # E.g. volume control commands.
@@ -24,16 +19,36 @@ class MulticastHelper:
     __MSG_PREFIX = 'piwall2_multicast_msg_start'
     __MSG_SUFFIX = 'piwall2_multicast_msg_end'
 
+    # 2 MB. This will be doubled to 4MB when we set it via setsockopt.
+    __VIDEO_SOCKET_RECEIVE_BUFFER_SIZE_BYTES = 2097152
+
+    # regarding socket.IP_MULTICAST_TTL
+    # ---------------------------------
+    # for all packets sent, after two hops on the network the packet will not
+    # be re-sent/broadcast (see https://www.tldp.org/HOWTO/Multicast-HOWTO-6.html)
+    __TTL = 2
+
     def __init__(self):
         self.__logger = Logger().set_namespace(self.__class__.__name__)
 
+        self.__setup_socket_receive_buffer_configuration()
+
         self.__send_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
-        self.__send_socket.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, self.TTL)
+        self.__send_socket.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, self.__TTL)
 
         self.__receive_video_socket = self.__make_receive_socket(self.ADDRESS, self.VIDEO_PORT)
 
         # set a higher timeout while we wait for the first packet of the video to be sent
         self.__receive_video_socket.settimeout(60)
+
+        # set a higher receive buffer size to avoid UDP packet loss.
+        # see: https://github.com/dasl-/piwall2/blob/main/docs/issues_weve_seen_before.adoc#udp-packet-loss
+        self.__receive_video_socket.setsockopt(
+            socket.SOL_SOCKET, socket.SO_RCVBUF, self.__VIDEO_SOCKET_RECEIVE_BUFFER_SIZE_BYTES
+        )
+        self.__logger.debug("Using receive buffer size of " +
+            self.__receive_video_socket.getsockopt(socket.SOL_SOCKET,socket.SO_RCVBUF) +
+            " bytes on receiver video socket.")
 
         self.__receive_control_socket = self.__make_receive_socket(self.ADDRESS, self.CONTROL_PORT)
 
@@ -76,3 +91,25 @@ class MulticastHelper:
         mreq = struct.pack("4sl", socket.inet_aton(address), socket.INADDR_ANY)
         sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
         return sock
+
+    # allow a higher receive buffer size to avoid UDP packet loss.
+    # see: https://github.com/dasl-/piwall2/blob/main/docs/issues_weve_seen_before.adoc#udp-packet-loss
+    def __setup_socket_receive_buffer_configuration(self):
+        max_socket_receive_buffer_size = (subprocess
+            .check_output(
+                "sudo sysctl --values net.core.rmem_max",
+                shell = True,
+                executable = '/usr/bin/bash',
+                stderr = subprocess.STDOUT
+            ).decode().strip()
+        )
+        max_socket_receive_buffer_size = int(max_socket_receive_buffer_size.decode().strip())
+        if max_socket_receive_buffer_size < self.__VIDEO_SOCKET_RECEIVE_BUFFER_SIZE_BYTES:
+            output = (subprocess
+                .check_output(
+                    f"sudo sysctl --write net.core.rmem_max={self.__VIDEO_SOCKET_RECEIVE_BUFFER_SIZE_BYTES}",
+                    shell = True,
+                    executable = '/usr/bin/bash',
+                    stderr = subprocess.STDOUT
+                ).decode().strip()
+            )
