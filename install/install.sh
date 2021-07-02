292fadf1347a2ce1de99e6c78a0d8a070562f73f
dasl- <david.leibovic@gmail.com>
2021-07-02 14:22:48 -0400

    install mbuffer
    Former-commit-id: e801fc5be450cb4fd425fbb5d6c5d252ee07001b

--- install/install.sh
+++ install/install.sh
@@ -18,7 +18,7 @@ main(){
 updateAndInstallPackages(){
     echo -e "\\nUpdating and installing packages..."
     sudo apt update
-    sudo apt -y install ffmpeg vlc omxplayer python3-pip fbi parallel dsh sshpass
+    sudo apt -y install ffmpeg vlc omxplayer python3-pip fbi parallel dsh sshpass mbuffer
     sudo apt -y full-upgrade
     sudo pip3 install --upgrade youtube_dl toml pytz
 }
