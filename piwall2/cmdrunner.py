3772c1324244ea190615fe494810e42190587587
dasl- <david.leibovic@gmail.com>
2021-09-27 21:42:27 -0400

    fix undefined var
    Former-commit-id: 6832d42a70dd9fa2e8cbc4723af36c4990cae0ca

--- piwall2/cmdrunner.py
+++ piwall2/cmdrunner.py
@@ -27,7 +27,7 @@ class CmdRunner:
 
         # populate self.__broadcaster_and_receivers_list
         broadcaster_hostname = self.get_broadcaster_hostname()
-        broadcaster_ip = socket.gethostbyname(self.__broadcaster)
+        broadcaster_ip = socket.gethostbyname(broadcaster_hostname)
         broadcaster_and_receivers_list = self.__receivers_list
         if (
             broadcaster_hostname not in self.__broadcaster_and_receivers_list and
