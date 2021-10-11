8aed7b17b21419f46744dadcae5ad0c246f9a574
dasl- <david.leibovic@gmail.com>
2021-10-10 21:30:57 -0400

    install npm and node in a better way?
    Former-commit-id: 1c210f84c884f9f45eb4830aa90d45098336b6d1

--- install/install_dependencies.sh
+++ install/install_dependencies.sh
@@ -3,8 +3,6 @@
 set -eou pipefail
 
 BASE_DIR="$(dirname "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )")"
-RESTART_REQUIRED_FILE='/tmp/piwall2_install_restart_required'
-is_restart_required=false
 installation_type=false
 only_install_python_deps=false
 
@@ -22,11 +20,6 @@ main(){
 
     if [[ "$installation_type" != "receiver" ]]; then
         installNode
-    fi
-
-    if [ "$is_restart_required" = true ] ; then
-        echo "Please restart to complete installation!"
-        touch "$RESTART_REQUIRED_FILE"
     fi
 }
 
@@ -72,15 +65,8 @@ parseOpts(){
 
 updateAndInstallAptPackages(){
     echo -e "\\nUpdating and installing apt packages..."
-
-    # installing and upgrading npm from scratch required a restart / re-login for the shell to recognize the new version
-    # when the version changed between `apt install npm` and `npm install npm@latest -g`
-    if ! which npm >/dev/null ; then
-        is_restart_required=true
-    fi
-
     sudo apt update
-    sudo apt -y install ffmpeg vlc omxplayer python3-pip fbi parallel dsh sshpass mbuffer npm sqlite3
+    sudo apt -y install ffmpeg vlc omxplayer python3-pip fbi parallel dsh sshpass mbuffer sqlite3
     sudo apt -y full-upgrade
     sudo pip3 install --upgrade youtube_dl yt-dlp toml pytz
 }
@@ -101,8 +87,12 @@ clearYoutubedlCache(){
 installNode(){
     echo -e "\\nInstalling latest version of node and its app dependencies..."
 
+    # install node and npm
+    curl -fsSL https://deb.nodesource.com/setup_16.x | sudo bash -
+    sudo apt-get install -y nodejs
+
     # The `apt install npm` command installs a very old version of npm. Use npm to upgrade itself to latest.
-    sudo npm install npm@latest -g
+    # sudo npm install npm@latest -g
 
     # Install app dependencies
     sudo npm install --prefix "$BASE_DIR/app"
