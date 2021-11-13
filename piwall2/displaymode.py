dd138379d9c964b20743931a229de23eb2eef304
dasl- <david.leibovic@gmail.com>
2021-11-13 16:38:43 -0500

    fix circular import
    Former-commit-id: 697e852381dfc1b6aafc1a8a37b3afa7b2fda290

--- piwall2/displaymode.py
+++ piwall2/displaymode.py
@@ -1,4 +1,4 @@
-from piwall2.broadcaster.settingsdb import SettingsDb
+import piwall2.broadcaster.settingsdb
 
 class DisplayMode:
     
@@ -9,13 +9,16 @@ class DisplayMode:
     DISPLAY_MODES = (DISPLAY_MODE_TILE, DISPLAY_MODE_REPEAT)
 
     def __init__(self):
-        self.__settings_db = SettingsDb()
+        self.__settings_db = piwall2.broadcaster.settingsdb.SettingsDb()
 
     # store display_mode settings in DB
     def update_db(self, display_mode_by_tv_id):
         db_data = {}
         for tv_id, display_mode in display_mode_by_tv_id.items():
-            db_key = self.__settings_db.make_tv_key_for_setting(SettingsDb.SETTING_DISPLAY_MODE, tv_id)
+            db_key = self.__settings_db.make_tv_key_for_setting(
+                piwall2.broadcaster.settingsdb.SettingsDb.SETTING_DISPLAY_MODE,
+                tv_id
+            )
             db_data[db_key] = display_mode
         success = self.__settings_db.set_multi(db_data)
         return success
