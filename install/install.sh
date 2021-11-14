b905d55682aa8e91f56ca5809f608c47a35ec0a9
dasl- <david.leibovic@gmail.com>
2021-11-14 16:38:18 -0500

    see if increasing gpu_mem fixes occasional video freezes
    Former-commit-id: ec2d30fc2be2effcf266c1308c90225d67067c48

--- install/install.sh
+++ install/install.sh
@@ -265,13 +265,13 @@ maybeAdjustScreenRotateMode(){
 setGpuMem(){
     gpu_mem=$(vcgencmd get_mem gpu | sed -n 's/gpu=\(.*\)M/\1/p')
     if (( gpu_mem < 128 )); then
-        echo 'Increasing gpu_mem to 128 megabytes...'
+        echo 'Increasing gpu_mem to 256 megabytes...'
 
         # comment out existing gpu_mem.* lines in config
         sudo sed $CONFIG -i -e "s/^\(gpu_mem.*\)/#\1/"
 
         # create the new stanza
-        echo 'gpu_mem=128' | sudo tee -a $CONFIG >/dev/null
+        echo 'gpu_mem=256' | sudo tee -a $CONFIG >/dev/null
     else
         echo "gpu_mem was large enough already: $gpu_mem megabytes..."
     fi
