3bf0ff83df034d61b94d133553803163a06cbb7c
dasl- <david.leibovic@gmail.com>
2021-12-03 22:28:39 -0500

    rain animation mode
    Former-commit-id: 62810362b5e7778ca5d597134267b4dcb26ab475

--- piwall2/animator.py
+++ piwall2/animator.py
@@ -183,7 +183,7 @@ class Animator:
         else:
             column_number = ((self.__ticks - 1) % num_columns)
             column_tv_ids = self.__config_loader.get_wall_columns()[column_number]
-            row_number = ((self.__ticks - 1) % num_rows)
+            row_number = (math.floor((self.__ticks - 1) / num_rows) % num_rows)
             row_tv_ids = self.__config_loader.get_wall_rows()[row_number]
             row_column_intersection_tv_ids = []
             for tv_id in column_tv_ids:
