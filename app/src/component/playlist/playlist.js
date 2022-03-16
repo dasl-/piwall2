cfd3609539c9285208afdf315cc9a7dd21926e51
dasl- <david.leibovic@gmail.com>
2022-03-16 03:04:03 -0400

    fix styling in mobile view
    Former-commit-id: 4828f589b9d2f97fb82644d824c21bd3a38fc45b

--- app/src/component/playlist/playlist.js
+++ app/src/component/playlist/playlist.js
@@ -63,7 +63,7 @@ class Playlist extends React.Component {
 
               <div className="play-queue">
                 { (this.props.videos.length === 0) &&
-                  <div className='container pt-2 px-0 mt-2 playlist-video'>
+                  <div className='container pt-2 px-0 mt-2 playlist-video-common'>
                     <div className="py-3 text-center">
                       &lt;Nothing&gt;
                     </div>
