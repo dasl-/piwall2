12c1ba6ea083284ee5869ca69249a582e9c2420a
dasl- <david.leibovic@gmail.com>
2022-04-29 02:21:36 -0400

    apt retries didnt work
    Former-commit-id: e69c515334e07767cd8abdec6107473e88ec220f

--- install/install_dependencies.sh
+++ install/install_dependencies.sh
@@ -86,9 +86,6 @@ stopPiwallServices(){
 
 updateAndInstallAptPackages(){
     info "\\nUpdating and installing apt packages..."
-
-    # attempt to work around for transient apt failures. Examples of failures: https://gist.github.com/dasl-/12c765db73d63c7936204903d0f0cf6b
-    echo 'APT::Acquire::Retries "30";' | sudo tee /etc/apt/apt.conf.d/piwall-retries >/dev/null
     sudo apt update
     sudo apt -y install ffmpeg vlc python3-pip fbi parallel dsh sshpass mbuffer sqlite3 pv
     sudo apt -y full-upgrade
