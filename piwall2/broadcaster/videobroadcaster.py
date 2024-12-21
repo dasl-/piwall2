7c3fcd382ee1937e743515d8fd8c84d3770280bc
dasl- <david.leibovic@gmail.com>
2024-12-21 18:53:00 -0500

    test

--- piwall2/broadcaster/videobroadcaster.py
+++ piwall2/broadcaster/videobroadcaster.py
@@ -246,8 +246,23 @@ class VideoBroadcaster:
 
         # Mix the best audio with the video and send via multicast
         # See: https://github.com/dasl-/piwall2/blob/main/docs/best_video_container_format_for_streaming.adoc
+        #
+        # Use `pv` to rate limit how fast we send the video. This is especially important when playing back
+        # local files. Without `pv`, they may send as fast as network bandwidth permits, which would prevent
+        # control messages from being received in a timely manner. Without `pv` here, when playing local files,
+        # we observed that a control message could be sent over the network and received ~10 seconds later --
+        # a delay because the tubes were clogged.
+        #
+        # Another reason `pv` is important is because the transmitted video can get corrupted if it is sent
+        # too fast. Specifically, there may be missing chunks of the video -- the receiver won't receive all
+        # of the bytes that the broadcaster sent. This corruption might occur on ~50% of the sends. Example
+        # output without the rate limit:
+        # https://gist.githubusercontent.com/dasl-/d06329d31df346b936419e394d364bc7/raw/7097647283435e888e8d5e1896e4472c7578273c/gistfile1.txt
+        #
+        # With the rate limit, all of the bytes are received. Example output with the rate limit:
+        # https://gist.githubusercontent.com/dasl-/cf10aa4da8d47a96c219e38d2bcfd6d8/raw/152895f07f16717cf678f981ad40c4bc9ffebe91/gistfile1.txt
         video_broadcast_cmd = ("set -o pipefail && export SHELLOPTS && " +
-            f"tee >({burst_throttling_clause}) >({broadcasting_clause}) >/dev/null")
+            f"pv --rate-limit 4M | tee >({burst_throttling_clause}) >({broadcasting_clause}) >/dev/null")
         self.__logger.info(f"Running broadcast command: {video_broadcast_cmd}")
 
         # Info on start_new_session: https://gist.github.com/dasl-/1379cc91fb8739efa5b9414f35101f5f
