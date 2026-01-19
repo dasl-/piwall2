c9d7b69470310d29d3482ad0ef018e1db91f6726
dleibovic <dleibovic@etsy.com>
2026-01-19 01:38:05 -0500

    fix the fact that /usr/local/bin isnt on roots path when using non-interactive shell

--- utils/update_yt-dlp.sh
+++ utils/update_yt-dlp.sh
@@ -3,12 +3,15 @@
 # Script that is run via cron to update yt-dlp.
 # Youtube releases updates every once in a while that breaks yt-dlp. If we don't constantly update
 # to the latest yt-dlp version, the pifi will stop working.
+#
+# By using uv to install yt-dlp, we can install yt-dlp even when the OS's python version
+# is too old to be supported by yt-dlp. See: https://github.com/dasl-/piwall2/issues/31
 
 echo "starting update_yt-dlp at $(date -u)"
-uv tool install 'yt-dlp[default]@latest'
+/usr/local/bin/uv tool install 'yt-dlp[default]@latest'
 
 # symlink yt-dlp to a place that's on our path by default
-sudo ln -sf /home/pi/.local/bin/yt-dlp /usr/local/bin/yt-dlp
+sudo ln -sf /home/pi/.local/bin/yt-dlp /usr/bin/yt-dlp
 
 # Just in case the yt-dlp cache got polluted, as it has before...
 # https://github.com/ytdl-org/youtube-dl/issues/24780
